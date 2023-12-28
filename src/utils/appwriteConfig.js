2efa8cceaf4e8bee55cbb72c3a0842052c2b6e6f
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-12-28 17:27:37 +0530

    Trying with corsproxy.io

--- src/utils/appwriteConfig.js
+++ src/utils/appwriteConfig.js
@@ -2,7 +2,7 @@ import { Client, Databases, ID, Account, Permission, Role } from "appwrite";
 
 const client = new Client();
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setEndpoint("https://corsproxy.io/https://cloud.appwrite.io/v1")
   .setProject(process.env.REACT_APP_DOCUMENT_ID);
 
 export const databases = new Databases(client);
